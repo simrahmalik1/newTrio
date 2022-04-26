32d1562551e0ad40ce7f4ecbf6aea7507323ede9
simrahmalik1 <100705032+simrahmalik1@users.noreply.github.com>
2022-04-26 00:01:37 -0400

    Added basic student form

---
+++ TrioWebsite/TrioWebsite/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace TrioWebsite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtRamID = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMajor = new System.Windows.Forms.TextBox();
+            this.txtStanding = new System.Windows.Forms.TextBox();
+            this.txtCurrentGPA = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtHomeAddress = new System.Windows.Forms.TextBox();
+            this.txtStudentStatus = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(129, 244);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(146, 270);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Click += new System.EventHandler(this.LblMessage_Click);
+            // 
+            // txtRamID
+            // 
+            this.txtRamID.AccessibleName = "txtRamID";
+            this.txtRamID.Location = new System.Drawing.Point(166, 3);
+            this.txtRamID.Name = "txtRamID";
+            this.txtRamID.Size = new System.Drawing.Size(158, 20);
+            this.txtRamID.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox10.Location = new System.Drawing.Point(3, 191);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(157, 20);
+            this.textBox10.TabIndex = 8;
+            this.textBox10.Text = "Student Status";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox9.Location = new System.Drawing.Point(3, 171);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(157, 20);
+            this.textBox9.TabIndex = 7;
+            this.textBox9.Text = "Home Address";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox8.Location = new System.Drawing.Point(3, 151);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(157, 20);
+            this.textBox8.TabIndex = 6;
+            this.textBox8.Text = "Phone Number";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox7.Location = new System.Drawing.Point(3, 131);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(157, 20);
+            this.textBox7.TabIndex = 5;
+            this.textBox7.Text = "Email";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox6.Location = new System.Drawing.Point(3, 111);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(157, 20);
+            this.textBox6.TabIndex = 4;
+            this.textBox6.Text = "Current GPA";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox3.Location = new System.Drawing.Point(3, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(157, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Last Name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox4.Location = new System.Drawing.Point(3, 71);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(157, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Major";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Location = new System.Drawing.Point(3, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "First Name";
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "RAM ID";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox5.Location = new System.Drawing.Point(3, 91);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(157, 20);
+            this.textBox5.TabIndex = 2;
+            this.textBox5.Text = "Standing";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.AccessibleName = "txtFirstName";
+            this.txtFirstName.Location = new System.Drawing.Point(166, 27);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(158, 20);
+            this.txtFirstName.TabIndex = 10;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.AccessibleName = "txtLastName";
+            this.txtLastName.Location = new System.Drawing.Point(166, 51);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(158, 20);
+            this.txtLastName.TabIndex = 11;
+            // 
+            // txtMajor
+            // 
+            this.txtMajor.AccessibleName = "txtMajor";
+            this.txtMajor.Location = new System.Drawing.Point(166, 71);
+            this.txtMajor.Name = "txtMajor";
+            this.txtMajor.Size = new System.Drawing.Size(158, 20);
+            this.txtMajor.TabIndex = 12;
+            // 
+            // txtStanding
+            // 
+            this.txtStanding.AccessibleName = "txtStanding";
+            this.txtStanding.Location = new System.Drawing.Point(166, 91);
+            this.txtStanding.Name = "txtStanding";
+            this.txtStanding.Size = new System.Drawing.Size(158, 20);
+            this.txtStanding.TabIndex = 13;
+            // 
+            // txtCurrentGPA
+            // 
+            this.txtCurrentGPA.AccessibleName = "txtCurrentGPA";
+            this.txtCurrentGPA.Location = new System.Drawing.Point(166, 111);
+            this.txtCurrentGPA.Name = "txtCurrentGPA";
+            this.txtCurrentGPA.Size = new System.Drawing.Size(158, 20);
+            this.txtCurrentGPA.TabIndex = 14;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(166, 131);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(158, 20);
+            this.txtEmail.TabIndex = 15;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(166, 151);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(158, 20);
+            this.txtPhoneNumber.TabIndex = 16;
+            // 
+            // txtHomeAddress
+            // 
+            this.txtHomeAddress.Location = new System.Drawing.Point(166, 171);
+            this.txtHomeAddress.Name = "txtHomeAddress";
+            this.txtHomeAddress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHomeAddress.Size = new System.Drawing.Size(158, 20);
+            this.txtHomeAddress.TabIndex = 17;
+            // 
+            // txtStudentStatus
+            // 
+            this.txtStudentStatus.Location = new System.Drawing.Point(166, 191);
+            this.txtStudentStatus.Name = "txtStudentStatus";
+            this.txtStudentStatus.Size = new System.Drawing.Size(158, 20);
+            this.txtStudentStatus.TabIndex = 18;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.txtStudentStatus, 1, 9);
+            this.tableLayoutPanel1.Controls.Add(this.txtHomeAddress, 1, 8);
+            this.tableLayoutPanel1.Controls.Add(this.txtPhoneNumber, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.txtEmail, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.txtCurrentGPA, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.txtStanding, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtMajor, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtLastName, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtFirstName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox6, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBox7, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.textBox8, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.textBox9, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.textBox10, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.txtRamID, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(96, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(327, 209);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.TableLayoutPanel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtRamID;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMajor;
+        private System.Windows.Forms.TextBox txtStanding;
+        private System.Windows.Forms.TextBox txtCurrentGPA;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtHomeAddress;
+        private System.Windows.Forms.TextBox txtStudentStatus;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ TrioWebsite/TrioWebsite/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TrioWebsite
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection c = new SqlConnection("Data Source=CSSERVER2; Initial Catalog=colojc7; User Id=colojc7; Password=1020997334;");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            c.Open();
+            SqlCommand cmd = new SqlCommand("insert into STUDENT values('"+txtRamID.Text+"','"+txtFirstName.Text+"','"+txtLastName.Text+"','"+txtMajor.Text+ "','" + txtStanding.Text + "','" + txtCurrentGPA.Text + "','" + txtEmail.Text + "','" + txtPhoneNumber.Text + "','" + txtHomeAddress.Text + "','" + txtStudentStatus.Text + "')", c);
+            cmd.ExecuteNonQuery();
+            lblMessage.Text = "Record submitted successfully!";
+            c.Close();
+        }
+
+        private void LblMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
